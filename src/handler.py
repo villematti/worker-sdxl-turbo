f41b36f49004eba293f4f5e06151a41fbbdd0658
Ville-Matti Hakanpää <villematti@villematti.info>
2025-07-08 22:35:14 +0300

    Add new model

--- src/handler.py
+++ src/handler.py
@@ -11,7 +11,7 @@ import time
 # You will want models to be loaded into memory before starting serverless.
 
 try:
-    pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
+    pipe = AutoPipelineForText2Image.from_pretrained("./RealVisXL_V5.0_Lightning_fp16.safetensors", torch_dtype=torch.float16, variant="fp16")
     pipe.to("cuda")
 except RuntimeError:
     quit()
